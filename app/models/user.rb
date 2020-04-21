6e39bb19169ffddb2c2701762c62cb3c7dbc0cb0
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-21 21:03:58 +0700

    downcase email be4 save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-
   validates_presence_of :name, :email
   validates_uniqueness_of :name, :email
 
+  before_save :downcase_email
+
   has_secure_password
+
+  private
+
+  def downcase_email
+    byebug
+    self.email.downcase!
+  end
 end
