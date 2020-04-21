364f208de4d55c0de69c71691d9fa2b3eb3b72db
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-21 20:30:00 +0700

    create password digest for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   validates_presence_of :name, :email
   validates_uniqueness_of :name, :email
+
+  has_secure_password
 end
