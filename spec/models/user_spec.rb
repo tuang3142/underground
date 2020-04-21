cea8407018d30e06c230ee5d15a607b855c69b2a
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-21 15:09:02 +0700

    test user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    user = User.new(name: 'Alice', email: 'alice@gmail.com')
+    @user = User.new(name: 'Alice', email: 'alice@gmail.com')
   end
 
-  it 'must have name and email' do
-    it { expect(user).to validate_presence_of(:name) }
-    it { expect(user).to validate_presence_of(:email) }
+  it { expect(@user).to be_valid }
+
+  context 'must have name and email' do
+    it { expect(@user).to validate_presence_of(:name) }
+    it { expect(@user).to validate_presence_of(:email) }
   end
+  
 end
