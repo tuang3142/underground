88e06a183a2c7b7cef7b1bee045d6f2de319a662
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-24 20:46:57 +0700

    get signup to user new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
   describe "GET /new" do
     before { get :new }
+
+    it { should route(:get, '/signup').to(action: :new) }
 
     it { should render_template('new') }
   end
