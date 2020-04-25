cb5c2d13c4f6bc4f9222fbd604a06e39d460b24e
Tuan Nguyen <tuan.nguyenviet271@gmail.com>
2020-04-25 19:54:20 +0700

    Make rubocop happy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
+
   validates :email, presence: true, uniqueness: true
 
   before_save :downcase_email
@@ -9,6 +10,6 @@ class User < ApplicationRecord
   private
 
   def downcase_email
-    self.email.downcase!
+    email.downcase!
   end
 end
