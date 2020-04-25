097554f9e1fea228e95a0de4f2043cacad6a878b
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-25 14:16:08 +0700

    remove name from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_132842) do
+ActiveRecord::Schema.define(version: 2020_04_25_070032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
