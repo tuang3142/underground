50707808971bba2cb25df69c223b4dca68882666
nguyenviettun <tuan.nguyenviet271@gmail.com>
2020-04-25 15:11:10 +0700

    add more tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
-  describe "GET /new" do
+  describe 'GET /new' do
     before { get :new }
 
     it { should route(:get, '/signup').to(action: :new) }
 
     it { should render_template('new') }
+  end
+
+  describe 'POST /create' do
+    before do
+      @params = {
+        user: {
+          email: 'alice@wonderland',
+          password: 'foobar'
+        }
+      }
+      post :create, params: @params
+    end
+
+    it { should permit(:email, :password).for(:create, params: @params).on(:user) }
+
+    it { should redirect_to root_path}
   end
 end
