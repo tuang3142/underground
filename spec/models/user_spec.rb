32bdc0a5e1aea5c39780ec7b48cb7d6c01efe764
Tuan Nguyen <tuan.nguyenviet271@gmail.com>
2020-04-26 19:32:38 +0700

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:email) }
+  it { is_expected.to have_many :microposts }
 
-  it { should validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:email) }
 
-  it { should validate_uniqueness_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
 
-  it { should validate_length_of(:password).is_at_least(6) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
 end
