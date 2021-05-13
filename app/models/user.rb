1c3e0275e14e95dcb085dbe6919ced174da88f7f
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-05-13 10:19:50 +0700

    Clean up unused part

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,46 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
   has_many :microposts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
-
-  # before_save :downcase_email
-
-  # has_secure_password
-
-  # def remember
-  #   token = new_token
-  #   update_attribute(:remember_digest, digest(token))
-  # end
-
-  # def forget
-  #   update_attribute(:remember_digest, nil)
-  # end
-
-  # def authenticated?(remember_token)
-  #   return false if remeber_token.nil?
-
-  #   BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  # end
-
-  # private
-
-  # def digest(string)
-  #   cost = if ActiveModel::SecurePassword.min_cost
-  #     BCrypt::Engine::MIN_COST
-  #   else
-  #     BCrypt::Engine.cost
-  #   end
-  #   BCrypt::Password.create(string, cost: cost)
-  # end
-
-  # def new_token
-  #   SecureRandom.urlsafe_base64
-  # end
-
-  # def downcase_email
-  #   email&.downcase!
-  # end
 end
